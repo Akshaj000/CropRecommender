4577affa1006b1c9967df2f48e0b57a077d92f52
Akshaj000 <akshaj000@gmail.com>
2022-01-10 21:45:55 +0530

    Added notes about Naive Bayes

--- croprecommender.py
+++ croprecommender.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np 
 from sklearn.ensemble import RandomForestClassifier
 import pyinputplus as pyip
+from sklearn.naive_bayes import GaussianNB
 
 
 cropdata = pd.read_csv("Crop_recommendation.csv")
@@ -18,6 +19,7 @@ This program gives the most suitable crop from 'rice' 'maize' 'chickpea' 'kidney
 'cotton''jute' 'coffee' based on the given features i.e. N, P, K, Temperature, Humidity, pH, Rainfall
 ''')
 print()
+
 print("Enter Features : ")
 Ka = pyip.inputInt("K : ")
 Ni = pyip.inputInt("N : ")
@@ -27,25 +29,46 @@ ph = pyip.inputFloat("pH : ")
 rainfall = pyip.inputFloat("Rainfall : ")
 temperature = pyip.inputFloat("Temperature : ")
 
+features = cropdata[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
+target = cropdata['label']
+x_test = [[ Ni, Po,Ka,temperature, humidity, ph, rainfall]]
+print(x_test)
 
 print("--------------------------------------------------------------------------------------------------------------------")
 print("Using random forest")
 print()
 print("Training from Crop_recommendation.csv")
 
-features = cropdata[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
-target = cropdata['label']
 
 RF = RandomForestClassifier(n_estimators=20, random_state=0)
 RF.fit(features,target)
-
-x_test = [[ Ni, Po,Ka,temperature, humidity, ph, rainfall]]
 
 print()
 print("predicting.....")
 print()
 
-predicted_value = RF.predict(x_test)
-print("Best crop Suitable for this condition is : ",predicted_value[0])
+Rpredicted_values = RF.predict(x_test)
+print("Best crop Suitable for this condition is : ",Rpredicted_values[0])
 
 print("--------------------------------------------------------------------------------------------------------------------")
+
+
+print("--------------------------------------------------------------------------------------------------------------------")
+print("Using Naïve Bayes")
+print()
+print("Training from Crop_recommendation.csv")
+
+NaiveBayes = GaussianNB()
+NaiveBayes.fit(features,target)
+
+print()
+print("predicting.....")
+print()
+
+Npredicted_values = NaiveBayes.predict(x_test)
+
+print("Best crop Suitable for this condition is : ",Npredicted_values[0])
+print("--------------------------------------------------------------------------------------------------------------------")
+
+if (str(Npredicted_values[0]) == str(Rpredicted_values[0])):
+    print("Suitable crop is : ", Npredicted_values[0])
